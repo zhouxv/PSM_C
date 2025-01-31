292bf0fb0301c349d4f331fb5d66b5f33c8a24be
zhouxv <1598284508@qq.com>
2025-01-31 18:36:13 +0800

    初始化构造

---
+++ execute.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 需要安装的包，libboost-all-dev libfmt-dev pkg-config libglib2.0-dev libgmp-dev libssl-dev libntl-dev
+
+cd extern
+wget -O boost_1_79_0.tar.gz https://sourceforge.net/projects/boost/files/boost/1.79.0/boost_1_79_0.tar.gz/download
+tar xzvf boost_1_79_0.tar.gz
+cd boost_1_79_0
+./bootstrap.sh --prefix=$(pwd)/boost_install --with-libraries=all
+./b2
+./b2 install --with=all
+cd ..
+
+git clone https://github.com/shahakash28/2PC-Circuit-PSI.git
+cd 2PC-Circuit-PSI
+mkdir build && cd build
+CC=/usr/bin/gcc-9 CXX=/usr/bin/g++-9 cmake ..
+cp ../aux_hash/* ../extern/HashingTables/cuckoo_hashing/.
+make
